c682f2ea2468f19c2770bd84ba1a8bae56b53527
Jon Vegard Venås <jonvegard89@yahoo.com>
2021-04-30 11:56:23 +0200

    changes

--- Sula/convertCoords.py
+++ Sula/convertCoords.py
@@ -1,4 +1,5 @@
 from pyproj import Transformer
+import pandas as pd
 import click
 import numpy as np
 @click.command()
@@ -10,12 +11,16 @@ def main(originx,originy):
     transformer = Transformer.from_crs(proj32,proj33)
 
     height = 70 # Height of bridge at middle point
+    # Data for 'SulaNW (Kvitneset)', 'SulaNE (Trælboneset)', 'SulaSW (Langeneset)', 'SulaSE (Kårsteinen)'
     Boom1 = np.array([6.1,6.1,4.4,3.6])
     Sensorh = np.array([[92.5,71.5,44.5],[76.8, 48.3, 27.3],[94.8, 75.0, 50.0, 27.0],[62.8, 40.0, 13.4]],dtype=object)
     BoomOrient = np.array([[72,74,74],[289, 290, 290],[81, 81, 81, 81],[223, 223, 223]],dtype=object)
     CoordUTM32 = np.array([[6924741, 345142],[6925267, 348347],[6920740, 346520],[6922074, 351140]])
+    CoordUTM32 = np.array([[6924741, 345142],[6925263.08,348346.72],[6920736.2,346525.95],[6922076.51,351139.85]])
     masth = np.array([100.5, 78.0,97.0,63.0])
-    mastb = np.array([6.0,6.0,14.0,12.0])
+    #mastb = np.array([6.0,6.0,14.0,12.0])
+    mastb = np.array([6.0,14.0,6.0,12.0])
+    mastb = np.array([8.0,12.2,1.8,13.9])
     f = open("POINT_FILE", "w")
     noPoints = sum([len(listElem) for listElem in Sensorh])+len(Sensorh)//2
     f.write("%d\n" % noPoints)
@@ -38,6 +43,21 @@ def main(originx,originy):
         f.write("%f,%f,%f,%f\n" % (x-originx,y-originy,mastb[i],masth[i]))
 
     f.close()
+    
+    mastNames = ['Kvitneset', 'Traelboneset','Langeneset','Kaarsteinen']
+    for i in range(len(masth)):
+        df = pd.DataFrame()
+        for j in range(len(Sensorh[i])):
+            z = np.floor(Sensorh[i][j]).astype(int)
+            filename = 'measurements/'+'10hz_'+mastNames[i]+'_60mnturbulence_statistics_'+str(z)+'_202011.csv'
+            df_all = pd.read_csv(filename)
+            df = df.append(df_all[df_all.date=='2020-11-19 06:00'])
+        df = df.reset_index()
+        df['coordsZ'] = mastb[i]+Sensorh[i]
+        df.to_csv('VelocityProfile_'+mastNames[i]+'.csv')
+
+
+
 
 if __name__ == '__main__':
     main()
