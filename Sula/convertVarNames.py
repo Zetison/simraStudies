5d05016451ef79a49a81a83f67d74a0a8b90aae0
Jon Vegard Venås <jonvegard89@yahoo.com>
2021-05-06 11:20:31 +0200

    Added script

---
+++ Sula/convertVarNames.py
@@ -0,0 +1,24 @@
+import click
+from typing import IO
+import vtk
+
+@click.command()
+@click.argument('infile', type=click.File('r'))
+
+def main(infile: IO):
+    reader = vtk.vtkUnstructuredGridReader()  # or vtkXMLStructuredGridReader, or vtkUnstructuredReader, or vtkStructuredReader
+    reader.SetFileName(infile.name)
+    reader.Update()
+    grid = reader.GetOutput()
+    for oldName, newName in zip(['k','U'],['tk','u']): 
+        data_tk = grid.GetPointData().GetArray(oldName)
+        data_tk.SetName(newName)
+        grid.GetPointData().AddArray(data_tk)
+    
+    writer = vtk.vtkUnstructuredGridWriter()
+    writer.SetFileName(infile.name[:-4]+'_new.vtk')
+    writer.SetInputData(grid)
+    writer.Write()
+
+if __name__ == '__main__':
+    main()
